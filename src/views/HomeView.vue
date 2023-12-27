b7ff7aadd35d8a04df1f68c8c3124dddd26202a3
tony <tony@teamplus.com.tw>
2023-12-27 13:47:32 +0800

    測試

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,23 @@
 <script setup lang="ts">
 import TheWelcome from '../components/TheWelcome.vue'
+import { useCounterStore } from '@/stores/counter'
+import { storeToRefs } from 'pinia'
+const store = useCounterStore()
+const { increment } = store
+const { count, doubleCount } = storeToRefs(store)
 </script>
 
 <template>
   <main>
     <TheWelcome />
+    <button @click="increment" class="button">++</button>
+    <br />
+    {{ count }}
+    {{ doubleCount }}
   </main>
 </template>
+<style lang="scss">
+.button {
+  @apply border p-1.5 m-2 bg-blue-500 rounded-xl;
+}
+</style>
