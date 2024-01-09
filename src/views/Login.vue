f5642fb4282907fe1b211ec07609e63ecefaffdf
tony <tony@teamplus.com.tw>
2024-01-09 18:00:30 +0800

    加入logo 切板調整

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,12 +1,14 @@
 <template>
   <div class="login">
-    <div class="loginPicture">
-      <div class="logo">
-        <h1>享樂酒店</h1>
-      </div>
-      <div class="background"></div>
+    <div class="logo">
+      <div class="logoImage"></div>
     </div>
-    <div class="loginArea">456</div>
+    <div class="loginWrapper">
+      <div class="loginPicture">
+        <div class="background"></div>
+      </div>
+      <div class="loginArea"></div>
+    </div>
   </div>
 </template>
 
@@ -14,24 +16,26 @@
 
 <style lang="scss" scoped>
 .login {
-  @apply flex;
-  .loginPicture {
-    @apply w-3/6;
-    .logo {
-      @apply h-32 bg-black;
-      h1 {
-        @apply text-white;
-      }
-    }
-    .background {
-      height: calc(100vh - 128px);
-      background-image: url('../assets/login/register.png');
-      background-position: center;
-      background-size: cover;
+  .logo {
+    @apply h-32 bg-black  px-4 py-8;
+    .logoImage {
+      @apply h-16 bg-no-repeat bg-left bg-contain;
+      background-image: url('../assets/login/logo.png');
     }
   }
-  .loginArea {
-    @apply w-3/6;
+  .loginWrapper {
+    @apply flex;
+    .loginPicture {
+      @apply w-3/6;
+      .background {
+        height: calc(100vh - 128px);
+        background-image: url('../assets/login/register.png');
+        @apply bg-cover bg-center;
+      }
+    }
+    .loginArea {
+      @apply w-3/6;
+    }
   }
 }
 </style>
