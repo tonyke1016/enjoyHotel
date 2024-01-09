e3a811c8f5daee36b34fe66d0c0761bc3a38ed45
tony <tony@teamplus.com.tw>
2024-01-09 17:24:55 +0800

    login切板

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,23 +1,24 @@
 <script setup lang="ts">
 import TheWelcome from '../components/TheWelcome.vue'
+import { useRouter } from 'vue-router'
 import { useCounterStore } from '@/stores/counter'
 import { storeToRefs } from 'pinia'
-const store = useCounterStore()
-const { increment } = store
-const { count, doubleCount } = storeToRefs(store)
+const router = useRouter()
+const goLogin = () => {
+  router.push('/login')
+}
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-    <button @click="increment" class="button">++</button>
-    <br />
-    {{ count }}
-    {{ doubleCount }}
-  </main>
+  <div class="homePage">
+    我是首頁
+    <button class="loginButton" @click="goLogin">前往登入頁</button>
+  </div>
 </template>
 <style lang="scss">
-.button {
-  @apply border p-1.5 m-2 bg-blue-500 rounded-xl;
+.homePage {
+  .loginButton {
+    @apply p-4 bg-blue-500;
+  }
 }
 </style>
---
+++ src/views/Login.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="login">
+    <div class="loginPicture">
+      <div class="logo">
+        <h1>享樂酒店</h1>
+      </div>
+      <div class="background"></div>
+    </div>
+    <div class="loginArea">456</div>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style lang="scss" scoped>
+.login {
+  @apply flex;
+  .loginPicture {
+    @apply w-3/6;
+    .logo {
+      @apply h-32 bg-black;
+      h1 {
+        @apply text-white;
+      }
+    }
+    .background {
+      height: calc(100vh - 128px);
+      background-image: url('../assets/login/register.png');
+      background-position: center;
+      background-size: cover;
+    }
+  }
+  .loginArea {
+    @apply w-3/6;
+  }
+}
+</style>
