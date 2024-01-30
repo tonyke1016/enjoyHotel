fd98f8b6a8bb1ee5a519c070badf8e19c5a599ae
Frank hsu <gary80519@yahoo.com.tw>
2024-01-30 15:17:53 +0800

    feat .Footer backup

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,48 @@
 <template>
-    <div class="bg-black">
-        <img src="../assets/footer/bi_line.png" alt="">
-        <img src="../assets/footer/Group.png" alt="">
+  <div class="bg-black text-white">
+    <div class="container flex justify-between test">
+      <div class="footer_leftSide">
+        <img src="../assets/login/logo.png" alt="logo" />
+        <div class="mt-10">
+          <img src="../assets/footer/bi_line.png" alt="" />
+          <img src="../assets/footer/Group.png" alt="" />
+        </div>
+      </div>
+      <div class="footer_rightSide">
+        <div class="flex items-end">
+          <div class="mr-20">
+            <h5>TEL</h5>
+            <span class="">+886-7-1234567</span>
+          </div>
+          <div class="">
+            <h5>MAIL</h5>
+            <span class="">elh@hexschool.com</span>
+          </div>
+        </div>
+        <div class="flex items-end">
+          <div class="mr-20">
+            <h5>FAX</h5>
+            <span class="">+886-7-1234567</span>
+          </div>
+          <div class="">
+            <h5>WEB</h5>
+            <span class="">www.elhhexschool.com.tw</span>
+          </div>
+        </div>
+      </div>
     </div>
+    <div class="container flex justify-between mt-20">
+      <span class="">806023 台灣高雄市新興區六角路123號</span>
+      <span class="">© 享樂酒店 2023 All Rights Reserved.</span>
+    </div>
+  </div>
 </template>
+
+<style lang="scss" scoped>
+img {
+  max-height: 100%;
+}
+.test{
+    max-width: 960px;
+}
+</style>
